45eb4fe95bbdce8a9c214d49da41f5e31b369329
Piotr Sampławski <sampi60@gmail.com>
2014-12-05 21:41:19 +0100

    allow empty price

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   validates :name, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :price, allow_nil: true, numericality: { greater_than_or_equal_to: 0.01 }
 
   has_attached_file :photo, styles: { medium: "300x300>",
                                        thumb:  "100x100>",
